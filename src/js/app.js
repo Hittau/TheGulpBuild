c2dd90f250b6f527c4c45dfda7473694e9c3e928
Hittau <maksimsraud@gmail.com>
2022-05-20 22:47:48 +0300

    improvement base header structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
-// burger
+// burger and menu
 const burger = document.querySelector('.burger')
 const wrapper = document.querySelector('.wrapper')
+const menu = document.querySelector('.menu')
 
 burger.onclick = _ => {
+	menu.classList.toggle('active')
 	burger.classList.toggle('active')
-	wrapper.classList.toggle('scroll_block')
+	wrapper.classList.toggle('scroll-block')
 }
