a51b20bdcb5080a7e33de5c32416d592601ca813
Hittau <maksimsraud@gmail.com>
2022-05-21 19:51:07 +0300

    best commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-// burger and menu
+// Открытие меню при клике на бургер
 const burger = document.querySelector('.burger')
 const wrapper = document.querySelector('.wrapper')
 const menu = document.querySelector('.menu')
@@ -9,21 +9,35 @@ burger.onclick = _ => {
 	wrapper.classList.toggle('scroll-block')
 }
 
-// dynamic adapt
-const headerBtn = document.querySelector('.header__btn')
-const parent = document.querySelector('.header__container')
+// динамический адаптив
+// перебрасывание кнопки в меню
+// const headerBtn = document.querySelector('.header__btn')
+// const parent = document.querySelector('.header__container')
 
-window.addEventListener('resize', event => {
-	const screenWidth = window.screen.width
-	if (screenWidth <= 767.98) {
-		if (!headerBtn.classList.contains('adapted')) {
-			menu.insertBefore(headerBtn, menu[4])
-			headerBtn.classList.add('adapted')
-		}
-	} else {
-		if (headerBtn.classList.contains('adapted')) {
-			parent.insertBefore(headerBtn, parent[2])
-			headerBtn.classList.remove('adapted')
+// window.addEventListener('resize', event => {
+// 	const screenWidth = window.screen.width
+// 	if (screenWidth <= 767.98) {
+// 		if (!headerBtn.classList.contains('adapted')) {
+// 			menu.insertBefore(headerBtn, menu[4])
+// 			headerBtn.classList.add('adapted')
+// 		}
+// 	} else {
+// 		if (headerBtn.classList.contains('adapted')) {
+// 			parent.insertBefore(headerBtn, parent[2])
+// 			headerBtn.classList.remove('adapted')
+// 		}
+// 	}
+// })
+
+// Закрытие меню при клике вне его
+document.addEventListener('click', e => {
+	const withinBoundaries =
+		e.composedPath().includes(menu) || e.composedPath().includes(burger)
+
+	if (menu.classList.contains('active')) {
+		if (!withinBoundaries) {
+			menu.classList.remove('active')
+			burger.classList.remove('active')
 		}
 	}
 })
